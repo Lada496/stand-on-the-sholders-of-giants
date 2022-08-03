394117d8cb06714779a75776f28e45ca662e9e99
Lada496 <67321065+Lada496@users.noreply.github.com>
2022-08-02 22:23:33 -0700

    leverage type predicate

--- crwn-clothing-redux-saga-typescript/src/store/categories/category.reducer.ts
+++ crwn-clothing-redux-saga-typescript/src/store/categories/category.reducer.ts
@@ -1,6 +1,11 @@
-import { CATEGORIES_ACTION_TYPES, Category } from "./category.types";
+import { AnyAction } from "redux";
+import { Category } from "./category.types";
 
-import { CategoryAction } from "./category.action";
+import {
+  fetchCategoriesStart,
+  fetchCategoriesSuccess,
+  fetchCategoriesFailure,
+} from "./category.action";
 
 export type CategoiresState = {
   readonly categories: Category[];
@@ -16,19 +21,16 @@ export const CATEGORIES_INITIAL_STATE: CategoiresState = {
 
 export const categoriesReducer = (
   state = CATEGORIES_INITIAL_STATE,
-  action = {} as CategoryAction
+  action = {} as AnyAction
 ): CategoiresState => {
-  switch (action.type) {
-    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START:
-      return {
-        ...state,
-        isLoading: true,
-      };
-    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS:
-      return { ...state, isLoading: false, categories: action.payload };
-    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED:
-      return { ...state, isLoading: false, error: action.payload };
-    default:
-      return state;
+  if (fetchCategoriesStart.match(action)) {
+    return { ...state, isLoading: true };
   }
+  if (fetchCategoriesSuccess.match(action)) {
+    return { ...state, isLoading: false, categories: action.payload };
+  }
+  if (fetchCategoriesFailure.match(action)) {
+    return { ...state, isLoading: false, error: action.payload };
+  }
+  return state;
 };
