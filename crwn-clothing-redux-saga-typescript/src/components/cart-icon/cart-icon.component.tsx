0b89398284a91784bd41fdf305e77c49d17c54a1
Lada496 <67321065+Lada496@users.noreply.github.com>
2022-09-02 22:24:57 -0700

    leaned how to import SVG in TypeScript

--- crwn-clothing-redux-saga-typescript/src/components/cart-icon/cart-icon.component.tsx
+++ crwn-clothing-redux-saga-typescript/src/components/cart-icon/cart-icon.component.tsx
@@ -4,21 +4,25 @@ import {
   selectCartCount,
   selectIsCartOpen,
 } from "../../store/cart/cart.selector";
-
 import { setIsCartOpen } from "../../store/cart/cart.action";
 
-import { ShopingIcon, CartIconContainer, ItemCount } from "./cart-icon.styles";
+// we need to declare global type to remove type error
+import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
+
+import { CartIconContainer, ItemCount } from "./cart-icon.styles";
 
 const CartIcon = () => {
   const dispatch = useDispatch();
+
   const cartCount = useSelector(selectCartCount);
   const isCartOpen = useSelector(selectIsCartOpen);
 
   const toggleIsCartOpen = () => dispatch(setIsCartOpen(!isCartOpen));
+
   return (
     <CartIconContainer onClick={toggleIsCartOpen}>
-      <ShopingIcon className="shopping-icon" />
-      <ItemCount className="item-count">{cartCount}</ItemCount>
+      <ShoppingIcon className="shopping-icon" />
+      <ItemCount>{cartCount}</ItemCount>
     </CartIconContainer>
   );
 };
