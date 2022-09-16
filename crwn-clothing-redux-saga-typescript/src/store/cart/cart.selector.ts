f5ef7bc831e7eba45f0625ddc78570fc6ce329ae
Lada496 <67321065+Lada496@users.noreply.github.com>
2022-09-16 16:39:09 -0700

    learned when to optimize for performance

--- crwn-clothing-redux-saga-typescript/src/store/cart/cart.selector.ts
+++ crwn-clothing-redux-saga-typescript/src/store/cart/cart.selector.ts
@@ -3,6 +3,13 @@ import { RootState } from "../store";
 import { CartState } from "./cart.reducer";
 const selectCartReducer = (state: RootState): CartState => state.cart;
 
+//incredibly important to know as a React developer: you only want to start improving the performance of your code or optimizing your code once you see there is a performance bottleneck i.e. if you do not notice any slow down in your code, there is no reason to start optimizing ahead of time
+// the reason for this is because optimizations have cost that is very important to know
+// Optimization is not free there are trade-offs: Optimization takes not only developers' time but also computer processing and resourcing time
+
+// good example: createSelector call has a cost on the memory and has cost on rendering time
+// it stores output value in memory somewhere and to do that storage also takes time
+// the reason we use it is we call cart state multiple times
 export const selectCartItems = createSelector(
   [selectCartReducer],
   (cart) => cart.cartItems
